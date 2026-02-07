7921838fb4ef3ed9affa31163b5ad447de88990a
Amanda <amanda.asgm@gmail.com>
2026-02-07 16:07:58 -0300

    feat(refunds): criando e listando registros por id e com autenticação e autorização

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,12 +1,81 @@
-import { Request, response, Response } from "express";
 import { prisma } from "@/database/prisma";
-import { z } from "zod";
+import { Request, Response } from "express";
 import { AppError } from "@/utils/AppError";
+import { z } from "zod";
+
+
+const CategoriesEnum = z.enum(["food", "others", "services", "transport", "accommodation"]);
 
 class RefundsController {
   async create(req: Request, res: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2, { message: "Informe o nome da solicitação" }),
+      category: CategoriesEnum,
+      amount: z.number().positive({ message: "Informe o valor do reembolso" }),
+      filename: z.string().min(20, { message: "Informe o nome do arquivo" }),
+    });
 
-    res.status(201).json({ message: "ok" });
+    const { name, category, amount, filename } = bodySchema.parse(req.body);
+
+    if (!req.user) {
+      throw new AppError("User not authenticated", 401);
+    }
+
+    const existingRefund = await prisma.refunds.findFirst({
+      where: {
+        name,
+        amount
+      }
+    });
+
+    if (existingRefund) {
+      throw new AppError("A refund with the same name and amount already exists", 400);
+    }
+
+    const refund = await prisma.refunds.create({
+      data: {
+        name,
+        category,
+        amount,
+        filename,
+        userId: req.user!.id 
+      }
+    });
+
+    res.status(201).json({ message: "ok", refund });
+  }
+
+  async list(req: Request, res: Response) {
+    if (!req.user) {
+      throw new AppError("User not authenticated", 401);
+    }
+
+    const querySchema = z.object({
+      name: z.string().optional().default(""),
+    });
+
+    const { name } = querySchema.parse(req.query);
+    
+    // somente o manager pode ver os reembolsos de todos os funcionários, mas o funcionário só pode ver os seus próprios reembolsos
+    const refunds = await prisma.refunds.findMany({
+      where: {
+        userId: req.user.role === "manager" ? undefined : req.user.id,
+        user: {
+            name: {
+              contains: name.trim(),
+          }
+        }
+      },
+      orderBy: { createdAt: "desc" },
+      include: {user: true}
+    });
+
+    // erro para caso nao exista o usuario buscado
+    if (refunds.length === 0) {
+      throw new AppError("No refunds found for the given name", 404);
+    }
+
+    res.json({ refunds });
   }
 }
 
--- src/routes/refunds-routes.ts
+++ src/routes/refunds-routes.ts
@@ -7,6 +7,9 @@ const refundsRoutes = Router();
 const refundsController = new RefundsController();
 
 // aqui eu defino quais roles estao autorizadas a acessar essa rota
-refundsRoutes.post('/', verifyUserAuthorization(["manager"]), refundsController.create); 
+refundsRoutes.post('/', verifyUserAuthorization(["employee"]), refundsController.create);
+// somente o manager pode ver os reembolsos de todos os funcionários, mas o funcionário só pode ver os seus próprios reembolsos
+refundsRoutes.get('/', verifyUserAuthorization(["employee","manager"]), refundsController.list);
+
 
 export { refundsRoutes };
